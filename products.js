7966da1fdf23b169cb779a0d2acc8696bf0f6548
Adel-Kazem <75915359+Adel-Kazem@users.noreply.github.com>
2025-03-23 12:27:57 +0200

    Add new product

--- products.js
+++ products.js
@@ -14,5 +14,13 @@ const PRODUCTS = [
     price: 67,
     image: "https://raw.githubusercontent.com/Adel-Kazem/subdomain-alpine-ex/main/products_images/belt-wolf-of-wallstreet-leonardo-di-caprio-v0-qd479m2ochjb1.webp",
     rating: 5
+  },
+  {
+    id: 5,
+    name: "Tempor et beatae ut",
+    description: "A non est aperiam es",
+    price: 14,
+    image: "https://raw.githubusercontent.com/Adel-Kazem/subdomain-alpine-ex/main/products_images/elle-leonardo-dicaprio-toast-inception-warner-bros-pictures_1.jpg",
+    rating: 5
   }
 ];
